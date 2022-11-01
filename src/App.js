172c8551969e1a41239d47abfd5b94ef1e934c43
ali-khoshqamat <ali_khoshghamat@yahoo.com>
2022-11-01 04:25:35 +0330

    create TodoList-App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TodoList App</h1>
+      <div className="container"></div>
+    </div>
+  );
+}
+
+export default App;
